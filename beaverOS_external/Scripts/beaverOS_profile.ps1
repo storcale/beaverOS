bea3c95df6ccf160295b14ea11fd2e7ebbdaf282
Storcale <105071746+storcale@users.noreply.github.com>
2024-07-25 18:26:01 +0200

    Update beaverOS_profile.ps1

--- beaverOS_external/Scripts/beaverOS_profile.ps1
+++ beaverOS_external/Scripts/beaverOS_profile.ps1
@@ -1,4 +1,4 @@
-$module_file = Get-ChildItem -Path 'C:\Users\' -Recurse -ErrorAction SilentlyContinue| Where-Object { $_.FullName -match '\\beaverOS_external\\Modules\\.*beaverOS_external.psm1$' } | Select-Object -ExpandProperty FullName
+$module_file = Get-ChildItem -Path 'E:\' -Recurse -ErrorAction SilentlyContinue| Where-Object { $_.FullName -match '\\beaverOS_external\\Modules\\.*beaverOS_external.psm1$' } | Select-Object -ExpandProperty FullName
 
 Import-Module($module_file)
 Write-Host("Welcome to Powershell! BeaverOS_external installed") -ForegroundColor Green
