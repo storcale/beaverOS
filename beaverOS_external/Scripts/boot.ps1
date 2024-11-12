e541b67801316d9da41370cad65f5f35df914780
Storcale <105071746+storcale@users.noreply.github.com>
2024-11-12 14:07:01 +0000

    fix boot.ps1

--- beaverOS_external/Scripts/boot.ps1
+++ beaverOS_external/Scripts/boot.ps1
@@ -1,5 +1,5 @@
 # Launch to manage beaverOS on the computer 
 $projectRoot = (Get-Item -Path $PSScriptRoot).Parent.FullName
-$module_path = Join-Path -Path $projectRoot -ChildPath "Modules\updater.psm1"
+$module_path = Join-Path -Path $projectRoot -ChildPath "Modules\manager.psm1"
 Import-Module($module_path)
-welcomeGUI
+install
